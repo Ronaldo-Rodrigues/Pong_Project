85494b8eed9cfeef4df2999224e6f1859a132643
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2021-08-15 21:43:14 -0300

    Paddles and Ball dinamic
    scripts de paddle,  player paddle com imput
    Dinamica de bola

---
+++ Pong_Project/Assets/Script/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    public float speed = 200.0f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        AddStartingForce();
+    }
+
+    private void AddStartingForce()
+    {
+        float x = Random.value < 0.5f ? -1.0f : 1.0f;
+        float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) :
+                                        Random.Range( 1.0f,  0.5f);
+
+        Vector2 direction = new Vector2(x, y);
+        _rb.AddForce(direction * speed); 
+    }
+}
---
+++ Pong_Project/Assets/Script/Paddle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    protected Rigidbody2D rb;
+
+    public float speed = 10.0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+}
